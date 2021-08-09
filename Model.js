841230a8cbcbe9e062a9d4e886e85f3f4361baaf
saraalshater <gleave.alshater@gmail.com>
2021-08-10 01:32:53 +0300

    done12

---
+++ Model.js
@@ -0,0 +1,70 @@
+// 'use strict';
+// const { Model } = require("mongoose");
+// const mongoose = require('mongoose');
+
+
+
+
+// const bookSchema = new mongoose.Schema({
+//     title: String,
+//     description: String,
+//     status: String,
+//     email: String
+// });
+
+
+// const Book = mongoose.model('Book', bookSchema);
+
+// function seedBookCollection() {
+
+//     const javaScript = new Book({
+
+//         title: 'JavaScript',
+//         description: 'Startup book to learn JavaScript',
+//         status: 'Educational',
+//         email: 'Gleave.alshater@hitmail.com'
+
+//     });
+
+//     const Html = new Book({
+
+//         title: 'Html',
+//         description: 'Startup book to learn HTML',
+//         status: 'Educational',
+//         email: 'Gleave.alshater@hotmail.com'
+
+//     });
+
+//     const Css = new Book({
+
+//         title: 'Css',
+//         description: 'Startup book to learn CSS',
+//         status: 'Educational',
+//         email: 'Gleave.alshater@hotmail.com'
+
+//     });
+
+//     javaScript.save();
+//     Html.save();
+//     Css.save();
+
+// }
+
+// // seedBookCollection();
+
+
+
+
+// // http://localhost:3001/book
+// function getBooksHandler(req,res) {
+    
+
+
+// }
+
+
+
+
+
+
+// module.exports = Model;
--- server.js
+++ server.js
@@ -1,15 +1,108 @@
 'use strict';
 
+const mongoose = require('mongoose');
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
+// const Model = require ('./Model');
 
 const app = express();
 app.use(cors());
 
+
 const PORT = process.env.PORT || 3001;
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+
+const bookSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  status: String,
+  email: String
+});
+
+
+
+
+
+const Book = mongoose.model('Book', bookSchema);
+
+
+
+
+function seedBookCollection() {
+
+  const javaScript = new Book({
+
+    title: 'JavaScript',
+    description: 'Startup book to learn JavaScript',
+    status: 'Educational',
+    email: 'Gleave.alshater@hitmail.com'
+
+  });
+
+  const Html = new Book({
+
+    title: 'Html',
+    description: 'Startup book to learn HTML',
+    status: 'Educational',
+    email: 'Gleave.alshater@hotmail.com'
+
+  });
+
+  const Css = new Book({
+
+    title: 'Css',
+    description: 'Startup book to learn CSS',
+    status: 'Educational',
+    email: 'Gleave.alshater@hotmail.com'
+
+  });
+
+  javaScript.save();
+  Html.save();
+  Css.save();
+
+}
+
+// seedBookCollection();
+
+
+
+app.get('/books', getBooksHandler);
+
+
+
+
+
+
+
+// http://localhost:3001/books
+function getBooksHandler(req, res) {
+
+
+  Book.find((err,bookArray) => {
+if(err){
+  res.send(err);
+}else{
+
+  res.send(bookArray);
+
+}
+  });
+
+
+
+}
+
+
+
+
+
 
 app.get('/test', (request, response) => {
 
